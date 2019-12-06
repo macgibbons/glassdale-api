1bab6664add87a99517442d7f542ee7a267ae120
macgibbons <macgibbons@gmail.com>
2019-12-06 15:20:20 -0600

    added hover styling on cards

--- scripts/criminals/CriminalList.js
+++ scripts/criminals/CriminalList.js
@@ -5,34 +5,7 @@ import CriminalComponent from "./Criminals.js";
 
 const eventHub = document.querySelector(".container")
 
-// const CriminalList = () => {
-//     // Load the application state to be used by this component
-//     const appStateCriminals = useCriminals()
 
-//     // What should happen when detective selects a crime?
-//     eventHub.addEventListener('crimeSelected', event => {
-//         // You remembered to add the id of the crime to the event detail, right?
-//         if ("crimeId" in event.detail) {
-//             /*
-//                 Filter the criminals application state down to the people that committed the crime
-//             */
-//             const matchingCriminals = appStateCriminals.filter()
-
-//             /*
-//                 Then invoke render() and pass the filtered collection as
-//                 an argument
-//             */
-//         }
-//     })
-
-//     ...
-// }
-
-// ConvictionSelect
-
-
-
-////
 const contentElement = document.querySelector(".criminalsContainer")
 
 const CriminalListComponent = () => {
@@ -55,7 +28,8 @@ const CriminalListComponent = () => {
     })
     let render = criminalCollection => {
         const crime = event.detail.crimeID
-        contentElement.innerHTML =
+        contentElement.innerHTML = ""
+        contentElement.innerHTML +=
             `
 
 
@@ -67,6 +41,7 @@ ${
     }
     </section>
     `
+    
     }
 
 }
